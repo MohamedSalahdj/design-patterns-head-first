6ae9f520952a7621bc537f043fb5674d6e59da16
Mohamed Salah <mohamedsalah@madaarsolutions.com>
2026-01-11 09:28:39 +0200

    Add ModelDuck and runtime behavior change demonstration

--- Behavioral-patterns/Strategy-Design-Pattern/03-strategy-pattern/main.py
+++ Behavioral-patterns/Strategy-Design-Pattern/03-strategy-pattern/main.py
@@ -1,13 +1,23 @@
-from ducks import MallardDuck, RubberDuck
+from ducks import MallardDuck, RubberDuck, ModelDuck
+
+from behaviors import FlyRocketPowered
 
 
 if __name__ == "__main__":
 
     d = [MallardDuck(), RubberDuck()]
-    
-    for duck in d: 
+
+    for duck in d:
         duck.display()
         duck.perform_fly()
         duck.perform_quack()
         duck.swim()
         print("--"*15)
+
+    # Change the behavior in runtime
+    model_duck = ModelDuck()
+    model_duck.perform_fly()
+    print("##"*15)
+
+    model_duck.set_fly_behavior(FlyRocketPowered())
+    model_duck.perform_fly()
